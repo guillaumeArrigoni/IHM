164364f3d595456054ae8488e8c3783615b88710
LorisDrid <loris.drid@etu.univ-cotedazur.fr>
2023-04-04 14:59:17 +0200

    resources

---
+++ app/src/main/java/com/example/tp6/FragmentNotifiable.java
@@ -0,0 +1,8 @@
+package com.example.tp6;
+
+import androidx.fragment.app.Fragment;
+
+public interface FragmentNotifiable {
+
+    void onFragmentNotify(Fragment fragment);
+}
--- app/src/main/java/com/example/tp6/MainActivity.java
+++ app/src/main/java/com/example/tp6/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.tp6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements PostExecuteActivity<Pokemon> {
+    private final String TAG = "frallo "+getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.go).setOnClickListener( clic -> {
+            String url = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/17d33dc111ffcc12b016d6485152aa3b1939c214/pokedex.json";
+            new HttpAsyncGet<>(url, Pokemon.class, this, new ProgressDialog(clic.getContext()) );
+        });
+    }
+
+
+    @Override
+    public void onPostExecutePokemons(List<Pokemon> itemList) {
+        Pokemon pokemonFirst = itemList.get(0);
+        Log.d(TAG,"First pokemon = " + pokemonFirst.getName());
     }
 }
